4a0d0944a7ce2a91002241746c54f0b8da610159
Aidan Baker <baker339@purdue.edu>
2022-06-22 16:53:08 -0400

    updated about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,6 +3,7 @@ import styles from "../styles/Home.module.css";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 import { NavBar } from "../components/nav";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
@@ -12,8 +13,36 @@ const Home: NextPage = () => {
       <NavBar currentTab="About" />
 
       <main className={styles.main}>
-        <div className={styles.maininside + " " + styles.cardnohover}>
+        <div
+          className={styles.maininside + " " + styles.cardnohover}
+          style={{ maxWidth: "100rem" }}
+        >
           <h1>About Us</h1>
+          <div>
+            <p>
+              Jeff Baker is a retired master electrician and a skilled carpenter
+              and handyman.
+            </p>
+            <p>
+              He enjoys woodworking as a hobby and is all around skilled at home
+              repair and maintenance.
+            </p>
+            <p>
+              Jeff Baker is located in the Brunswick Plantation community in
+              Calabash, NC
+            </p>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Link href="/contact">
+              <div className={styles.contactbutton}>Contact</div>
+            </Link>
+          </div>
         </div>
       </main>
 
