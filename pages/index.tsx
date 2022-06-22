a00e86ff2f39d025028fda146ae180568d923af8
Aidan Baker <baker339@purdue.edu>
2022-06-22 16:39:01 -0400

    updated index links

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-import styles from "../styles/Home.module.css";
 import { Footer } from "../components/footer";
 import { Header } from "../components/header";
 import { NavBar } from "../components/nav";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
@@ -11,45 +11,53 @@ const Home: NextPage = () => {
       <Header />
       <NavBar currentTab="Home" />
       <main className={styles.main}>
-        <Link href="/contact" className={styles.description}>
-          Contact Us
+        <Link href="/contact">
+          <a className={styles.description}>Contact Us</a>
         </Link>
         <div className={styles.grid}>
-          <Link href="/services" className={styles.card}>
-            <>
-              <h2>Maintenance &rarr;</h2>
-              <ul>
-                <li>Power Washing</li>
-                <li>Hedge Trimming</li>
-                <li>Changing Lightbulbs</li>
-                <li>Changing Alarm Batteries</li>
-              </ul>
-            </>
+          <Link href="/services">
+            <a className={styles.card}>
+              <>
+                <h2>Maintenance &rarr;</h2>
+                <ul>
+                  <li>Power Washing</li>
+                  <li>Hedge Trimming</li>
+                  <li>Changing Lightbulbs</li>
+                  <li>Changing Alarm Batteries</li>
+                </ul>
+              </>
+            </a>
           </Link>
-          <Link href="/services" className={styles.card}>
-            <>
-              <h2>Painting &rarr;</h2>
-              <ul>
-                <li>Interiors</li>
-                <li>Shutters</li>
-                <li>Lamp Posts</li>
-                <li>Mailboxes</li>
-              </ul>
-            </>
+          <Link href="/services">
+            <a className={styles.card}>
+              <>
+                <h2>Painting &rarr;</h2>
+                <ul>
+                  <li>Interiors</li>
+                  <li>Shutters</li>
+                  <li>Lamp Posts</li>
+                  <li>Mailboxes</li>
+                </ul>
+              </>
+            </a>
           </Link>
-          <Link href="/services" className={styles.card}>
-            <>
-              <h2>Misc. &rarr;</h2>
-              <ul>
-                <li>Minor Carpentry</li>
-                <li>Minor Plumbing</li>
-                <li>Gutter Cleaning</li>
-                <li>Gutter Guard Installation</li>
-              </ul>
-            </>
+          <Link href="/services">
+            <a className={styles.card}>
+              <>
+                <h2>Misc. &rarr;</h2>
+                <ul>
+                  <li>Minor Carpentry</li>
+                  <li>Minor Plumbing</li>
+                  <li>Gutter Cleaning</li>
+                  <li>Gutter Guard Installation</li>
+                </ul>
+              </>
+            </a>
           </Link>
-          <Link href="/services" className={styles.card}>
-            <h2>And More! &rarr;</h2>
+          <Link href="/services">
+            <a className={styles.card}>
+              <h2>And More! &rarr;</h2>
+            </a>
           </Link>
         </div>
       </main>
